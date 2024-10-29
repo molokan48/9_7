b05a0d78845ecd84d7a78ec47904e882dbfee8e0
alexander <molokan2007@mail.ru>
2024-10-29 22:55:53 +0300

    Update 9_7.py

--- 9_7.py
+++ 9_7.py
@@ -1,18 +1,18 @@
 
-
 def is_prime(f):
     def wrapper(*args):
         ret_func = f(*args)
-        if ret_func == 2 or ret_func == 3:
+        if ret_func <=3 :
             print("Простое")
             return ret_func
-        for i in range(3 , ret_func , 2):
-            if ret_func % 2 == 0:
-                print("Составное")
-                return ret_func
+        else:
+            for i in range(3 , int(ret_func)):
+                if  ret_func % i == 0:
+                    print("Составное")
+                    return ret_func
             else:
-                print("Простое")
-                return ret_func
+                    print("Простое")
+                    return ret_func
     return wrapper
 
 @is_prime
@@ -20,5 +20,5 @@ def sum_three(*args):
     r = sum(args)
     return r
 
-result = sum_three(2, 3, 8)
+result = sum_three(5, 0, 2)
 print(result)
